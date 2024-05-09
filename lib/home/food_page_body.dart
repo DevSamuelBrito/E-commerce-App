a3874db6f33dd8bcededdaa23c2ad9791579b25b
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-05-09 12:07:47 -0300

    create the icon_and_text_wigder file

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,7 @@
+import 'package:ecommerce_app/utils/colors.dart';
+import 'package:ecommerce_app/widgets/bid_text.dart';
+import 'package:ecommerce_app/widgets/icon_and_text_widget.dart';
+import 'package:ecommerce_app/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,12 +13,14 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
+      //color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position) {
           return _buildPageItem(position);
@@ -27,8 +33,8 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     return Stack(
       children: [
         Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
+          height: 260,
+          margin: EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -41,14 +47,64 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 40, right: 40,bottom: 10),
+            height: 130,
+            margin: EdgeInsets.only(left: 50, right: 50, bottom: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage("assets/image/food1.jpg"),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Sorvete"),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) =>
+                              Icon(Icons.star, color: AppColors.yellowColor),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: '4.5'),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: '1287'),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: 'comments'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: 'Normal',
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: '1.7km',
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_filled_rounded,
+                          text: '32min',
+                          iconColor: AppColors.iconColor2),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
